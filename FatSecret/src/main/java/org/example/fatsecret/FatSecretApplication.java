16b1c8c517de4c49647aad89f28c026c3fb0b3cc
galina <galina.krupitsa@gmail.com>
2025-11-14 16:18:34 +0500

    o

---
+++ FatSecret/src/main/java/org/example/fatsecret/FatSecretApplication.java
@@ -0,0 +1,13 @@
+package org.example.fatsecret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatSecretApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatSecretApplication.class, args);
+    }
+
+}
---
+++ FatSecret/src/main/java/org/example/fatsecret/User.java
@@ -0,0 +1,41 @@
+package org.example.fatsecret;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private Long id;
+    private String username;
+    private int age;
+    private int weight;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public int getAge() {
+        return age;
+    }
+    public int getWeight() {
+        return weight;
+    }
+
+}
