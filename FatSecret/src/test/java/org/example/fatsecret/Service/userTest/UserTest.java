d3aab153543720e93fc8628287b6f78186f6dba2
galina <galina.krupitsa@gmail.com>
2025-12-14 23:05:43 +0500

    test

--- FatSecret/src/test/java/org/example/fatsecret/Service/userTest/UserTest.java
+++ FatSecret/src/test/java/org/example/fatsecret/Service/userTest/UserTest.java
@@ -4,18 +4,14 @@ import org.example.fatsecret.Entity.User;
 import org.example.fatsecret.Repositories.FatRepository;
 import org.example.fatsecret.Service.UserService;
 import org.example.fatsecret.WeightedUsers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Arrays;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
